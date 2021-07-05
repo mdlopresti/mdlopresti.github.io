42c1791e4a67375b59cc8130d5c252ef854e1604
next.js <mdlopresti@users.noreply.github.com>
2021-07-05 15:26:09 +0000

    Updated static bundle

---
+++ docs/_next/static/zav7ZneLzeDvTNrAJ47rk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ea6f1f42cebc7b8cbe53.css","static/chunks/pages/index-ec4ddf52b7354bae65bf.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
