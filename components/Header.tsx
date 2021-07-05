d8711a69805e23971126d829ecd4d200c5c761cf
Michael LoPresti <mike@mdlopresti.dev>
2021-07-05 22:51:04 +0000

    adding CDN link

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,6 +8,12 @@ export default function Header() {
             <Head>
                 <title>Automation or Bust</title>
                 <meta name="description" content="A blog by on software and automation by Michael LoPresti" />
+                <link
+                    rel="stylesheet"
+                    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+                    crossorigin="anonymous"
+                />
             </Head>
             <header>
                 <Link href="/">
